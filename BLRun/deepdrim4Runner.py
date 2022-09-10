7bec45b751922cc5ba1821c178494ba42015ee3d
Shao-shan Carol Huang <shhuang1@gmail.com>
2022-09-10 17:31:04 -0400

    Rename deepdrim2Runner to deepdrim4Runner

--- BLRun/deepdrim4Runner.py
+++ BLRun/deepdrim4Runner.py
@@ -9,17 +9,17 @@ from sklearn.model_selection import GroupKFold
 add_bind = ''
 def generateInputs(RunnerObj):
     '''
-    Function to generate desired inputs for DeepDRIM.
+    Function to generate desired inputs for DeepDRIM2.
     If the folder/files under RunnerObj.datadir exist, 
     this function will not do anything.
 
     :param RunnerObj: An instance of the :class:`BLRun`
     '''
-    if not RunnerObj.inputDir.joinpath("DEEPDRIM").exists():
-        print("Input folder for DEEPDRIM does not exist, creating input folder...")
-        RunnerObj.inputDir.joinpath("DEEPDRIM").mkdir(exist_ok = False)
+    if not RunnerObj.inputDir.joinpath("DEEPDRIM2").exists():
+        print("Input folder for DEEPDRIM2 does not exist, creating input folder...")
+        RunnerObj.inputDir.joinpath("DEEPDRIM2").mkdir(exist_ok = False)
         
-    if not RunnerObj.inputDir.joinpath("DEEPDRIM/toPredict_pairsDEEPDRIM.txtTF_divide_pos.txt").exists():
+    if not RunnerObj.inputDir.joinpath("DEEPDRIM2/predict_pairsDEEPDRIM2.txtTF_divide_pos.txt").exists():
         # input expression data
         print('Expression data: '+str(RunnerObj.inputDir.joinpath(RunnerObj.exprData)))
         ExpressionData = pd.read_csv(RunnerObj.inputDir.joinpath(RunnerObj.exprData),
@@ -28,27 +28,27 @@ def generateInputs(RunnerObj):
         newExpressionData = ExpressionData.copy()
         
         # output expression .csv file (has header)
-        newExpressionData.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM/ExpressionData.csv"),
+        newExpressionData.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM2/ExpressionData.csv"),
                              sep = ',', header  = True, index = True)
 
         geneNameMap = pd.concat([ExpressionData.index.to_series().str.lower(),ExpressionData.index.to_series()],axis=1)
-        geneNameMap.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM/GeneName_map.txt"),
+        geneNameMap.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM2/GeneName_map.txt"),
                            sep='\t',header=False,index=False)
         
         # Read file for trueEdges and create positive pairs file (no header)
         print('True edges: '+str(RunnerObj.inputDir.joinpath(RunnerObj.trueEdges)))
         trueEdgesDF = pd.read_csv(RunnerObj.inputDir.joinpath(RunnerObj.trueEdges),
                                 sep = ',', header = 0, index_col = None)
-        trueEdgesDF.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM/PositivePairs.txt"),
+        trueEdgesDF.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM2/PositivePairs.txt"),
                                  sep=',',header=False,index=False)
         
         
         # relative to current path; current path is mounted as /ext3/data
-        #inputDir = str(RunnerObj.inputDir).split(str(Path.cwd()))[1] + "/DEEPDRIM"
-        inputDir = RunnerObj.inputDir.relative_to(Path.cwd()).joinpath('DEEPDRIM')
+        #inputDir = str(RunnerObj.inputDir).split(str(Path.cwd()))[1] + "/DEEPDRIM2"
+        inputDir = RunnerObj.inputDir.relative_to(Path.cwd()).joinpath('DEEPDRIM2')
 
         # Create training pairs, training pairs TF divide, gene name map files
-        # the function outputs to cwd, so enter into the DEEPDRIM directory to have the output files there
+        # the function outputs to cwd, so enter into the DEEPDRIM2 directory to have the output files there
         cmdToRun = ' '.join([
             'singularity exec --no-home',
             '-B ' + str(Path.cwd())+':/ext3/data/'+add_bind,
@@ -59,18 +59,18 @@ def generateInputs(RunnerObj):
             'python /ext3/DeepDRIM/generate_training_realdata.py ',
             '-expr_file '+'ExpressionData.csv',
             '-pos_pair_file '+'PositivePairs.txt',
-            '-label DEEPDRIM'
+            '-label DEEPDRIM2'
             '\"'])
         print(cmdToRun)
         os.system(cmdToRun)
 
         # Create the cross-validation fold divide file
         # n lines - dividing positions for n-1 factors
-        tfDividePos = np.genfromtxt(RunnerObj.inputDir.joinpath('DEEPDRIM/training_pairsDEEPDRIM.txtTF_divide_pos.txt'),
+        tfDividePos = np.genfromtxt(RunnerObj.inputDir.joinpath('DEEPDRIM2/training_pairsDEEPDRIM2.txtTF_divide_pos.txt'),
                                     delimiter=',',dtype='int')
         tfGroups = np.repeat(np.arange(len(tfDividePos)-1),np.diff(tfDividePos))
         group_kfold = GroupKFold(n_splits=3)
-        with open(RunnerObj.inputDir.joinpath('DEEPDRIM/training_pairsDEEPDRIM.txtCV_fold_divide.txt'),'w') as ofh:
+        with open(RunnerObj.inputDir.joinpath('DEEPDRIM2/training_pairsDEEPDRIM2.txtCV_fold_divide.txt'),'w') as ofh:
             split_it = group_kfold.split(X=np.zeros(len(tfGroups)),
                                          y=np.zeros(len(tfGroups)),groups=tfGroups)
             for train,test in split_it:
@@ -79,43 +79,43 @@ def generateInputs(RunnerObj):
                 ofh.write('\n')
 
         # Create the pairs to predict file; TF->all genes
-        trainingPairs = pd.read_csv(RunnerObj.inputDir.joinpath('DEEPDRIM/training_pairsDEEPDRIM.txt'),
+        trainingPairs = pd.read_csv(RunnerObj.inputDir.joinpath('DEEPDRIM2/training_pairsDEEPDRIM2.txt'),
                                     sep='\t',header=None,index_col=None)
         trainingEdgesFrom = trainingPairs.iloc[tfDividePos[:-1],0]
         allGenes = ExpressionData.index.to_series().str.lower()
-        pairsToPredict = pd.DataFrame(np.array(np.meshgrid(trainingEdgesFrom, allGenes, indexing='xy')).T.reshape(-1,2),
+        predictPairs = pd.DataFrame(np.array(np.meshgrid(trainingEdgesFrom, allGenes, indexing='xy')).T.reshape(-1,2),
                                       columns=['Gene1','Gene2'])
-        pairsToPredict['value'] = 3
-        pairsToPredict.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM/toPredict_pairsDEEPDRIM.txt"),
+        predictPairs['value'] = 3
+        predictPairs.to_csv(RunnerObj.inputDir.joinpath("DEEPDRIM2/predict_pairsDEEPDRIM2.txt"),
                                       sep='\t',header=False,index=False)
 
-        pairsToPredict['rnum'] = pairsToPredict.index
-        predictDividePos = [min(pairsToPredict.loc[pairsToPredict.Gene1==tf,'rnum']) for tf in trainingEdgesFrom]
-        predictDividePos.append(pairsToPredict.shape[0])
-        np.savetxt(RunnerObj.inputDir.joinpath("DEEPDRIM/toPredict_pairsDEEPDRIM.txtTF_divide_pos.txt"),predictDividePos,fmt='%d')
+        predictPairs['rnum'] = predictPairs.index
+        predictDividePos = [min(predictPairs.loc[predictPairs.Gene1==tf,'rnum']) for tf in trainingEdgesFrom]
+        predictDividePos.append(predictPairs.shape[0])
+        np.savetxt(RunnerObj.inputDir.joinpath("DEEPDRIM2/predict_pairsDEEPDRIM2.txtTF_divide_pos.txt"),predictDividePos,fmt='%d')
         
 def run(RunnerObj):
     '''
-    Function to run DEEPDRIM algorithm
+    Function to run DEEPDRIM2 algorithm
 
     :param RunnerObj: An instance of the :class:`BLRun`
     '''
     # current path will be mounted as /ext3/data
-    # '/ext3/data/inputs/L0/hESC/DEEPDRIM'
-    inputDir = Path('/ext3/data').joinpath(RunnerObj.inputDir.relative_to(Path.cwd()),'DEEPDRIM')
+    # '/ext3/data/inputs/L0/hESC/DEEPDRIM2'
+    inputDir = Path('/ext3/data').joinpath(RunnerObj.inputDir.relative_to(Path.cwd()),'DEEPDRIM2')
 
-    # outputs/L0/hESC/DEEPDRIM
+    # outputs/L0/hESC/DEEPDRIM2
     # make output dirs if they do not exist:
-    outDir = "outputs/"+str(RunnerObj.inputDir).split("inputs/")[1]+"/DEEPDRIM/"
+    outDir = "outputs/"+str(RunnerObj.inputDir).split("inputs/")[1]+"/DEEPDRIM2/"
     os.makedirs(outDir, exist_ok = True)
     
-    with open(RunnerObj.inputDir.joinpath('DEEPDRIM/training_pairsDEEPDRIM.txtTF_divide_pos.txt'), 'r') as fp:
+    with open(RunnerObj.inputDir.joinpath('DEEPDRIM2/training_pairsDEEPDRIM2.txtTF_divide_pos.txt'), 'r') as fp:
         numBatches = len(fp.readlines()) -1
 
     loadFromH5 = False
     TFOrderRandom = False
     if Path(outDir).joinpath('representation_train').exists():
-        print("Representation folder for DEEPDRIM training exists, removing representation training folder...")
+        print("Representation folder for DEEPDRIM2 training exists, removing representation training folder...")
         shutil.rmtree(Path(outDir).joinpath('representation_train'))
     cmdToRun = ' '.join([
         'singularity exec --no-home',
@@ -128,18 +128,18 @@ def run(RunnerObj):
         'python /ext3/DeepDRIM/generate_input_realdata.py ',
         '-out_dir representation_train',
         '-expr_file ',str(inputDir.joinpath('ExpressionData.csv')),
-        '-pairs_for_predict_file ',str(inputDir.joinpath('training_pairsDEEPDRIM.txt')),
-        '-geneName_map_file ',str(inputDir.joinpath('DEEPDRIM_geneName_map.txt')),
+        '-pairs_for_predict_file ',str(inputDir.joinpath('training_pairsDEEPDRIM2.txt')),
+        '-geneName_map_file ',str(inputDir.joinpath('DEEPDRIM2_geneName_map.txt')),
         '-flag_load_from_h5 '+str(loadFromH5),
         '-flag_load_split_batch_pos True',
-        '-TF_divide_pos_file '+str(inputDir.joinpath('training_pairsDEEPDRIM.txtTF_divide_pos.txt')),
+        '-TF_divide_pos_file '+str(inputDir.joinpath('training_pairsDEEPDRIM2.txtTF_divide_pos.txt')),
         '-TF_num '+str(numBatches), '-TF_order_random '+str(TFOrderRandom),
         '\"'])
     print(cmdToRun)
     os.system(cmdToRun)
 
     if Path(outDir).joinpath('representation_predict').exists():
-        print("Representation folder for DEEPDRIM predict exists, removing representation predict folder...")
+        print("Representation folder for DEEPDRIM2 predict exists, removing representation predict folder...")
         shutil.rmtree(Path(outDir).joinpath('representation_predict'))
     cmdToRun = ' '.join([
         'singularity exec --no-home',
@@ -152,11 +152,11 @@ def run(RunnerObj):
         'python /ext3/DeepDRIM/generate_input_realdata.py ',
         '-out_dir representation_predict',
         '-expr_file ',str(inputDir.joinpath('ExpressionData.csv')),
-        '-pairs_for_predict_file ',str(inputDir.joinpath('toPredict_pairsDEEPDRIM.txt')),
-        '-geneName_map_file ',str(inputDir.joinpath('DEEPDRIM_geneName_map.txt')),
+        '-pairs_for_predict_file ',str(inputDir.joinpath('predict_pairsDEEPDRIM2.txt')),
+        '-geneName_map_file ',str(inputDir.joinpath('DEEPDRIM2_geneName_map.txt')),
         '-flag_load_from_h5 '+str(loadFromH5),
         '-flag_load_split_batch_pos True',
-        '-TF_divide_pos_file '+str(inputDir.joinpath('toPredict_pairsDEEPDRIM.txtTF_divide_pos.txt')),
+        '-TF_divide_pos_file '+str(inputDir.joinpath('predict_pairsDEEPDRIM2.txtTF_divide_pos.txt')),
         '-TF_num '+str(numBatches), '-TF_order_random '+str(TFOrderRandom),
         '\"'])
     print(cmdToRun)
@@ -175,12 +175,12 @@ def run(RunnerObj):
         '-num_batches '+str(numBatches),
         '-data_path representation_train/version11/',
         '-output_dir .',
-        '-cross_validation_fold_divide_file '+str(inputDir.joinpath('training_pairsDEEPDRIM.txtCV_fold_divide.txt')),
+        '-cross_validation_fold_divide_file '+str(inputDir.joinpath('training_pairsDEEPDRIM2.txtCV_fold_divide.txt')),
         '\"'])
     print(cmdToRun)
     os.system(cmdToRun)
 
-    with open(RunnerObj.inputDir.joinpath('DEEPDRIM/training_pairsDEEPDRIM.txtCV_fold_divide.txt'),'r') as ifh:
+    with open(RunnerObj.inputDir.joinpath('DEEPDRIM2/training_pairsDEEPDRIM2.txtCV_fold_divide.txt'),'r') as ifh:
         cross_fold = []
         for line in ifh:
             line = line.strip()
@@ -212,19 +212,19 @@ def run(RunnerObj):
 
 def parseOutput(RunnerObj):
     '''
-    Function to parse outputs from DEEPDRIM.
+    Function to parse outputs from DEEPDRIM2.
 
     :param RunnerObj: An instance of the :class:`BLRun`
     '''
     # Quit if output directory does not exist
-    outDir = "outputs/"+str(RunnerObj.inputDir).split("inputs/")[1]+"/DEEPDRIM/"
+    outDir = "outputs/"+str(RunnerObj.inputDir).split("inputs/")[1]+"/DEEPDRIM2/"
     #if not Path(outDir+'outFile.txt').exists():
     #    print(outDir+'outFile.txt'+'does not exist, skipping...')
     #    return
     
-    geneNameMap = pd.read_csv(RunnerObj.inputDir.joinpath('DEEPDRIM/GeneName_map.txt'),
+    geneNameMap = pd.read_csv(RunnerObj.inputDir.joinpath('DEEPDRIM2/GeneName_map.txt'),
                               sep='\t',header=None,index_col=0,names=['name_to'])
-    with open(RunnerObj.inputDir.joinpath('DEEPDRIM/training_pairsDEEPDRIM.txtCV_fold_divide.txt'),'r') as ifh:
+    with open(RunnerObj.inputDir.joinpath('DEEPDRIM2/training_pairsDEEPDRIM2.txtCV_fold_divide.txt'),'r') as ifh:
         cross_fold = []
         for line in ifh:
             line = line.strip()
