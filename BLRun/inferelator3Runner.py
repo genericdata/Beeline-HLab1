8c6cc7a7ad9ca717e619727a6b81fd7e5541a3cc
Shao-shan Carol Huang <shhuang1@gmail.com>
2023-05-13 23:09:57 -0400

    Filter for unique entries in gold standard and prior edges

--- BLRun/inferelator3Runner.py
+++ BLRun/inferelator3Runner.py
@@ -5,12 +5,7 @@ from pathlib import Path
 import numpy as np
 import shutil
 
-from inferelator import utils
-from inferelator import workflow
-from inferelator import crossvalidation_workflow
-from inferelator.postprocessing import MetricHandler
-from inferelator.distributed.inferelator_mp import MPControl
-
+add_bind = ''
 def generateInputs(RunnerObj):
     algName = RunnerObj.name
     runDir = RunnerObj.params.get('run_dir','')
@@ -18,7 +13,7 @@ def generateInputs(RunnerObj):
         print("Input folder for "+algName+" "+runDir+" does not exist, creating input folder...")
         RunnerObj.inputDir.joinpath(algName,runDir).mkdir(exist_ok = False)
 
-    print('Expression data: '+str(RunnerObj.inputDir.joinpath(RunnerObj.exprData)))
+    print('Expression data: ',str(RunnerObj.inputDir.joinpath(RunnerObj.exprData)))
     ExpressionData = pd.read_csv(RunnerObj.inputDir.joinpath(RunnerObj.exprData),
                                  header = 0, index_col = 0)
     if not RunnerObj.inputDir.joinpath(algName,runDir,"ExpressionData.csv").exists():
@@ -26,52 +21,89 @@ def generateInputs(RunnerObj):
                              sep = '\t', header  = True, index = True)
 
     # reformat the true edges file to adj matrix
-    print('True edges: '+str(RunnerObj.inputDir.joinpath(RunnerObj.trueEdges)))
-    if not RunnerObj.inputDir.joinpath(algName,runDir,"gold_standard.tsv").exists():
-        gold_standard = pd.read_csv(RunnerObj.inputDir.joinpath(RunnerObj.trueEdges),sep=',')
+    print('Gold standard edges: ',RunnerObj.params['useGS'] if RunnerObj.params['useGS']=='False' else str(RunnerObj.inputDir.joinpath(algName,runDir,RunnerObj.params['useGS'])))
+    if (RunnerObj.params['useGS']!='False') and (not RunnerObj.inputDir.joinpath(algName,runDir,"gold_standard.tsv").exists()):
+        gold_standard = pd.read_csv(RunnerObj.inputDir.joinpath(algName,runDir,RunnerObj.params['useGS']),sep='\t')
         gold_standard['Type'] = gold_standard.get('Type',1)# some refNetworks.csv file do not have the type column
-        gold_standard = gold_standard.pivot(index="Gene2", columns="Gene1")
+        gold_standard = gold_standard.drop_duplicates(subset=['Gene1','Gene2','Type']).pivot(index="Gene2", columns="Gene1", values="Type")
         gold_standard[~pd.isna(gold_standard)] = 1.
         gold_standard = gold_standard.fillna(0).astype(int)
-        gold_standard.columns = gold_standard.columns.droplevel(0)
+        #gold_standard.columns = gold_standard.columns.droplevel(0)
         gold_standard = gold_standard.reindex(ExpressionData.index, axis=1).reindex(ExpressionData.index, axis=0).fillna(0).astype(int)
         gold_standard.to_csv(RunnerObj.inputDir.joinpath(algName,runDir,'gold_standard.tsv'),sep='\t')
 
     # reformat priors to adj matrix
-    print('True edges prior: '+
-          (RunnerObj.inputDir.joinpath(algName,runDir,RunnerObj.params['trueEdgesPrior']) if 'trueEdgesPrior' in RunnerObj.params else ''))
-    if ('trueEdgesPrior' in RunnerObj.params) and (not RunnerObj.inputDir.joinpath(algName,runDir,"priors.tsv").exists()):
-        priors = pd.read_csv(RunnerObj.inputDir.joinpath(algName,runDir,RunnerObj.params['trueEdgesPrior']),sep=',')
+    print('Prior edges: ',RunnerObj.params['usePrior'] if RunnerObj.params['usePrior']=='False' else str(RunnerObj.inputDir.joinpath(algName,runDir,RunnerObj.params['usePrior'])))
+    if (RunnerObj.params['usePrior']!='False') and (not RunnerObj.inputDir.joinpath(algName,runDir,"priors.tsv").exists()):
+        priors = pd.read_csv(RunnerObj.inputDir.joinpath(algName,runDir,RunnerObj.params['usePrior']),sep='\t')
         priors['Type'] = priors.get('Type',1)
-        priors = priors.pivot(index="Gene2", columns="Gene1")
+        priors = priors.drop_duplicates(subset=['Gene1','Gene2','Type']).pivot(index="Gene2", columns="Gene1", values="Type")
         priors[~pd.isna(priors)] = 1.
         priors = priors.fillna(0).astype(int)
-        priors.columns = priors.columns.droplevel(0)
+        #priors.columns = priors.columns.droplevel(0)
         priors = priors.reindex(ExpressionData.index, axis=1).reindex(ExpressionData.index, axis=0).fillna(0).astype(int)
         priors.to_csv(RunnerObj.inputDir.joinpath(algName,runDir,'priors.tsv'),sep='\t')
 
-def setup_workflow(in_dir, gs_file, tf_file, out_dir):
-        worker = workflow.inferelator_workflow(regression=METHOD, workflow="tfa")
-        worker.set_file_paths(input_dir=in_dir, output_dir=out_dir, expression_matrix_file=EXPR_FILE,
-                              gold_standard_file=gs_file, tf_names_file=tf_file)
-        worker.set_file_properties(expression_matrix_columns_are_genes=False)
-        worker.set_file_loading_arguments('expression_matrix_file', sep=",")
-        worker.set_run_parameters(num_bootstraps=5)
-        worker.set_output_file_names(nonzero_coefficient_file_name=None, pdf_curve_file_name=None,
-                                     curve_data_file_name=None)
-
-        return worker
-        
 def run(RunnerObj):
+    '''
+    Function to run INFERELATOR3 algorithm
+
+    :param RunnerObj: An instance of the :class:`BLRun`
+    '''
+    # current path will be mounted as /ext3/data
+    # '/ext3/data/inputs/L0/hESC/DEEPDRIM'
+    runDir = RunnerObj.params.get('run_dir','')
+    algName = RunnerObj.name
+    inputDirB = Path('/ext3/data').joinpath(RunnerObj.inputDir.relative_to(Path.cwd()),algName,runDir)
+
+    # outputs/L0/hESC/DEEPDRIM
+    # make output dirs if they do not exist:
+    outDir = "outputs/"+str(RunnerObj.inputDir).split("inputs/")[1]+"/"+algName+"/"+('' if runDir=='' else runDir+"/")
+    outDirB = Path('/ext3/data').joinpath(outDir)
+    os.makedirs(outDir, exist_ok = True)
     
-    worker = setup_workflow(sub_pp, sub_pp_gs_reformatted, sub_pp_tfs, out_dir)
-    worker.append_to_path('output_dir', "prior")
+    cmdToRun = ' '.join([
+        'singularity exec --no-home',
+        '-B ' + str(Path.cwd())+':/ext3/data/'+add_bind,
+        '--overlay ' + str(RunnerObj.singularityOverlay) + ':ro',
+        str(RunnerObj.singularityImage),
+        '/bin/bash -c \"source /ext3/env.sh; conda activate INFERELATOR3; ',
+        'cd',str(Path('/ext3/data')),';'
+        'command time -v -o '+str(outDirB.joinpath('time.txt')),
+        'python '+str(Path('/ext3/data').joinpath('Algorithms/INFERELATOR3/runInferelator3.py')),
+        '--method ',RunnerObj.params['method'],
+        '--workflow ',RunnerObj.params['workflow'],
+        '--in_dir ',str(inputDirB),
+        '--out_dir ',str(outDirB),
+        '--expr_file ',str(inputDirB.joinpath('ExpressionData.tsv')),
+        '--gs_file '+str(inputDirB.joinpath('gold_standard.tsv')) if RunnerObj.params['useGS']!='False' else '',
+        '--regulator_file ',str(inputDirB.joinpath(RunnerObj.params['regulators'])),
+        '--priors_file '+str(inputDirB.joinpath('priors.tsv')) if RunnerObj.params['usePrior']!='False' else '',
+        '--split_gs_for_cv '+RunnerObj.params['splitGSForCV'],
+        '--cv_split_ratio '+RunnerObj.params['CVSplitRatio'],
+        '--random_seed '+RunnerObj.params['randomSeed'],
+        '--num_bootstraps '+RunnerObj.params['numBootstraps'],
+        '\"'])
+    print(cmdToRun)
+    os.system(cmdToRun)
+
     
-    worker.set_file_paths(priors_file=sub_pp_gs_reformatted)
-    worker.set_crossvalidation_parameters(split_gold_standard_for_crossvalidation=True, cv_split_ratio=0.5)
-    worker.set_run_parameters(random_seed=k)
-    worker.get_data()
-                                          
 def parseOutput(RunnerObj):
-    deepdrimRunner.parseOutputForPredictFromBestModel(RunnerObj,'DEEPDRIM7',RunnerObj.inputDir,'auprc')
+    '''
+    Function to parse outputs from Inferelator3. 
+
+    :param RunnerObj: An instance of the :class:`BLRun`
+    '''
+    # Quit if output directory does not exist
+    runDir = RunnerObj.params.get('run_dir','')
+    algName = RunnerObj.name
+    outDir = "outputs/"+str(RunnerObj.inputDir).split("inputs/")[1]+"/"+algName+"/"+('' if runDir=='' else runDir+"/")
+
+    # Read output and reformat
+    netDF = pd.read_csv(Path(outDir).joinpath('network.tsv.gz'),compression='gzip',
+                        sep='\t',header=0)    
+    outDF = netDF[['regulator','target','combined_confidences']].copy()
+    outDF = outDF.sort_values(by=['combined_confidences','target','regulator'], ascending=[False,True,True])
+    outDF.to_csv(Path(outDir).joinpath('rankedEdges.csv'),
+                 sep='\t',index=False,header=['Gene1','Gene2','EdgeWeight'])
                                           
